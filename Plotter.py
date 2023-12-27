1495f2b1a79e84dd77c6b4781d2397fd21db88af
macq2 <mjedrzejowski00@gmail.com>
2023-12-27 12:57:26 +0100

    Version 1.3.1 - small corrections

--- Plotter.py
+++ Plotter.py
@@ -1,6 +1,7 @@
 import json
 import matplotlib.pyplot as plt
 from matplotlib.ticker import AutoLocator
+from datetime import datetime
 
 filename = 'data.json'
 
@@ -14,12 +15,15 @@ plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
 
 ax.plot(xvalues, yvalues, linewidth=3)
-ax.set_title('Size of the files', fontsize=22)
-ax.set_xlabel('Date [-]', fontsize=16)
+current_time = datetime.now()
+formatted_current_time = current_time.strftime('%d-%m-%Y %H:%M')
+ax.set_title(f"File sizes over time / data for {formatted_current_time.expandtabs(12)}", fontsize=22)
+ax.set_xlabel('Date [Y-m-d]', fontsize=16)
 ax.set_ylabel('File size [MB]', fontsize=16)
 
 ax.xaxis.set_major_locator(AutoLocator())
 ax.yaxis.set_major_locator(AutoLocator())
 ax.tick_params(axis='both', labelsize=12)
 
+plt.savefig('plot.png')
 plt.show()
